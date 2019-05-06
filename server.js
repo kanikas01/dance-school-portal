00145bacfbc31588c54d3426b49249fa52f3a906
kanikas01 <larvakurt@yahoo.com>
2019-05-05 19:09:50 -0700

    Add middleware, routes and db sync

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
+const db = require("./models");
+const compression = require("compression");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Middleware
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Add routes, both API and view
+app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -14,6 +25,21 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+const syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
