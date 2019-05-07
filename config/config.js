5fbc839c99a72eed69523cd717bbc7dc905f7275
kanikas01 <larvakurt@yahoo.com>
2019-05-07 09:45:00 -0700

    Put sequelize config info in config.js file instead of config.json

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: process.env.DB_DIALECT
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "testdb",
+    "host": "localhost",
+    "dialect": "mysql",
+    "logging": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+};
