f75167b6d6b6eb727c31ce44165df812c5544678
kanikas01 <larvakurt@yahoo.com>
2019-05-16 14:28:50 -0700

    Add ability to download CSV file of marketing contacts

--- client/src/components/Tabs/Marketing/Marketing.js
+++ client/src/components/Tabs/Marketing/Marketing.js
@@ -1,38 +1,49 @@
 import React, { Component,  } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 import userAPI from "../../../utils/userAPI";
+import { CSVLink, CSVDownload } from "react-csv";
 
 class Marketing extends Component {
 
   state = {
-    users: []
+    users: [],
+    marketingList: []
   };
 
   componentDidMount() {
     let queryString = "?onMarketingList=1"
     userAPI.searchUsers(queryString)
       .then(res => this.setState ({ users: res.data }) )
+      // Create marketing email list for download
+      .then( () => {
+        const newMarketingList = [["first name", "last name", "email"]];
+        for (var user of this.state.users) {
+          newMarketingList.push([user.firstName, user.lastName, user.email]);
+        }
+        this.setState ({ marketingList: newMarketingList });   
+      })
       .catch(err => console.log(err));
   }
 
   render () {
     return (
       <Container>
-        <h3>Marketing Tab Content</h3>
-        <h6>TODO</h6>
-        <ul>
-          <li>Get emails for people on marketing list</li>
-        </ul>
-
+        <CSVLink
+          filename={"dance-school-marketing-list.csv"}
+          className="btn btn-primary"
+          target="_blank"
+          data={this.state.marketingList}>
+          Download marketing email list
+        </CSVLink>
+        <hr/>
         <ListGroup>
-            {this.state.users.map((user, index) => (   
-              <ListGroup.Item key={user.id}>{user.firstName} {user.lastName} {user.email}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-
-
+          {this.state.users.map((user, index) => (   
+            <ListGroup.Item key={user.id}>{user.firstName} {user.lastName} {user.email}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       </Container>
     );
   }
