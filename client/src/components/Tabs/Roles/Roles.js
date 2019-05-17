795f85e1291151bf98d11b6627ed08ad980a22f2
kanikas01 <larvakurt@yahoo.com>
2019-05-16 17:36:15 -0700

    Revise navigation for Roles tab

--- client/src/components/Tabs/Roles/Roles.js
+++ client/src/components/Tabs/Roles/Roles.js
@@ -1,16 +1,17 @@
 import React, { Component,  } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+import Tab from 'react-bootstrap/Tab'
 import ListGroup from 'react-bootstrap/ListGroup'
 import roleAPI from "../../../utils/roleAPI";
 
 class Roles extends Component {
-
-  state = {
-    open: false,
-    roles: [],
-    devNull: ""
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      roles: []
+    };
+  }
 
   componentDidMount() {
     roleAPI.getRoles()
@@ -18,25 +19,31 @@ class Roles extends Component {
       .catch(err => console.log(err));
   }
 
-  handleDelete = (event, index) => {
-    event.preventDefault();
-    let role = this.state.roles[index];
-    roleAPI.deleteRole(role.id)
-      .then(res => {
-        this.setState ({ devNull: this.state.roles.splice(index, 1) });
-      })
-      .catch(err => console.log(err));
-  };
-
   render () {
-
     return (
       <Container>
-        <ListGroup>
-        {this.state.roles.map((role, index) => (    
-          <ListGroup.Item key={role.id}>{role.name}</ListGroup.Item>
-        ))}
-        </ListGroup>
+        <Tab.Container id="roles-tab-nav" defaultActiveKey="view-roles">
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="view-roles">View Roles</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="view-roles">
+                  <ListGroup>
+                    {this.state.roles.map((role, index) => (    
+                      <ListGroup.Item key={role.id}>{role.name}</ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
       </Container>
     );
   }
