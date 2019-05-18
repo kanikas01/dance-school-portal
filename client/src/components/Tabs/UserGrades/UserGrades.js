8ad73d62f940d204905bc650b74ac830854480b8
kanikas01 <larvakurt@yahoo.com>
2019-05-18 12:46:42 -0700

    Add basic UserGrades tab

---
+++ client/src/components/Tabs/UserGrades/UserGrades.js
@@ -0,0 +1,18 @@
+import React, { Component,  } from "react";
+
+class UserGrades extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h3>User Grades</h3>
+    );
+  }
+}
+
+
+
+export default UserGrades;
