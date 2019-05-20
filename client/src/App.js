6a820e8a9eb9c2f9910d8edc901ec1485c0400f8
kanikas01 <larvakurt@yahoo.com>
2019-05-20 10:42:01 -0700

    Create Login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
 import Navigation from "./components/Navigation";
 import Header from './components/Header';
+import Login from './pages/Login'
 import NoMatch from './pages/NoMatch'
 import Portal from './pages/Portal'
 import Welcome from './pages/Welcome'
@@ -21,6 +22,7 @@ class App extends Component {
           <Router>
             <Switch>
               <Route exact path="/" />
+              <Route exact path="/login" component={Login}/>
               <Route exact path="/portal" component={() => (<Portal role={this.state.role} />)} />
               <Route component={NoMatch} />
             </Switch>
