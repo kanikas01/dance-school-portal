ddaa7900cbbfb01e2bdb125d6b568a25e5dc82b6
kanikas01 <larvakurt@yahoo.com>
2020-02-14 19:46:04 -0800

    Update UserProfileForm to functional component, fix bug where forms would not re-render when swapping users of same role

--- client/src/components/UserProfileForm/UserProfileForm.js
+++ client/src/components/UserProfileForm/UserProfileForm.js
@@ -1,135 +1,138 @@
-import React, { Component, } from "react";
-import userAPI from "../../utils/userAPI";
+import React from 'react';
+import userAPI from '../../utils/userAPI';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-class UserProfileForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userId: this.props.userId,
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      isActive: false,
-      onMarketingList: false,
-      roleName: "",
-      roleId: ""
-    };
+function UserProfileForm({ userId }) {
+  const [currentUser, setCurrentUser] = React.useState({
+    userId: userId,
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    isActive: false,
+    onMarketingList: false,
+    roleName: '',
+    roleId: ''
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  componentDidMount() {
-    userAPI.getUser(this.state.userId)
-      .then(res => this.setState({
-        firstName: res.data.firstName,
-        lastName: res.data.lastName,
-        email: res.data.email,
-        password: res.data.password,
-        isActive: res.data.isActive,
-        onMarketingList: res.data.onMarketingList,
-        roleName: res.data.Role.name,
-        roleId: res.data.Role.id,
-      }))
+  React.useEffect(() => {
+    userAPI
+      .getUser(userId)
+      .then(res =>
+        setCurrentUser({
+          ...currentUser,
+          firstName: res.data.firstName,
+          lastName: res.data.lastName,
+          email: res.data.email,
+          password: res.data.password,
+          isActive: res.data.isActive,
+          onMarketingList: res.data.onMarketingList,
+          roleName: res.data.Role.name,
+          roleId: res.data.Role.id
+        })
+      )
       .catch(err => console.log(err));
-  }
+  }, [userId]);
 
-  handleInputChange(event) {
+  function handleInputChange(event) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
 
     if (name) {
-      this.setState({
-        [name]: value
-      });
+      setCurrentUser({ ...currentUser, [name]: value });
     }
   }
 
-  handleFormSubmit = event => {
+  function handleFormSubmit(event) {
     event.preventDefault();
-    userAPI.updateUser(this.state.userId, {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email,
-      password: this.state.password,
-      RoleId: this.state.roleId,
-      isActive: this.state.isActive,
-      onMarketingList: this.state.onMarketingList
-    })
-      .then(res => alert("User saved!"))
+    userAPI
+      .updateUser(userId, {
+        firstName: currentUser.firstName,
+        lastName: currentUser.lastName,
+        email: currentUser.email,
+        password: currentUser.password,
+        RoleId: currentUser.roleId,
+        isActive: currentUser.isActive,
+        onMarketingList: currentUser.onMarketingList
+      })
+      .then(res => alert('User saved!'))
       .catch(err => console.log(err));
-  };
-
-  render() {
-    return (
-      <div>
-        <h3>User Info</h3>
-        <Form>
-          <Form.Group controlId="formGroupAddFirstName">
-            <Form.Label>First name</Form.Label>
-            <Form.Control
-              name="firstName"
-              value={this.state.firstName}
-              type="name"
-              onChange={this.handleInputChange}
-              placeholder="" />
-          </Form.Group>
-          <Form.Group controlId="formGroupAddLastName">
-            <Form.Label>Last name</Form.Label>
-            <Form.Control
-              name="lastName"
-              type="name"
-              value={this.state.lastName}
-              onChange={this.handleInputChange}
-              placeholder="" />
-          </Form.Group>
-          <Form.Group controlId="formGroupAddEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              name="email"
-              type="email"
-              value={this.state.email}
-              onChange={this.handleInputChange}
-              placeholder="" />
-          </Form.Group>
-          <Form.Group controlId="formGroupAddPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              name="password"
-              type="password"
-              value={this.state.password}
-              onChange={this.handleInputChange}
-              placeholder="" />
-          </Form.Group>
-          <Form.Group controlId="formGroupAddIsActiveCheckbox">
-            <Form.Check
-              name="isActive"
-              type="checkbox"
-              onChange={this.handleInputChange}
-              label="Is Active"
-              checked={this.state.isActive} />
-          </Form.Group>
-          <Form.Group controlId="formGroupAddMarketingCheckbox">
-            <Form.Check
-              name="onMarketingList"
-              type="checkbox"
-              onChange={this.handleInputChange}
-              label="On Marketing List"
-              checked={this.state.onMarketingList} />
-          </Form.Group>
-          <Form.Group>
-            <Button
-              onClick={this.handleFormSubmit}>
-              Submit
-            </Button>
-          </Form.Group>
-        </Form>
-      </div>
-    );
   }
+
+  return (
+    <>
+      {currentUser.firstName && (
+        <div>
+          <h3>User Info</h3>
+          <Form>
+            <Form.Group controlId="formGroupAddFirstName">
+              <Form.Label>First name</Form.Label>
+              <Form.Control
+                name="firstName"
+                value={currentUser.firstName}
+                type="name"
+                onChange={handleInputChange}
+                placeholder=""
+              />
+            </Form.Group>
+            <Form.Group controlId="formGroupAddLastName">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control
+                name="lastName"
+                type="name"
+                value={currentUser.lastName}
+                onChange={handleInputChange}
+                placeholder=""
+              />
+            </Form.Group>
+            <Form.Group controlId="formGroupAddEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                name="email"
+                type="email"
+                value={currentUser.email}
+                onChange={handleInputChange}
+                placeholder=""
+              />
+            </Form.Group>
+            <Form.Group controlId="formGroupAddPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                type="password"
+                value={currentUser.password}
+                onChange={handleInputChange}
+                placeholder=""
+              />
+            </Form.Group>
+            <Form.Group controlId="formGroupAddIsActiveCheckbox">
+              <Form.Check
+                name="isActive"
+                type="checkbox"
+                onChange={handleInputChange}
+                label="Is Active"
+                checked={currentUser.isActive}
+              />
+            </Form.Group>
+            <Form.Group controlId="formGroupAddMarketingCheckbox">
+              <Form.Check
+                name="onMarketingList"
+                type="checkbox"
+                onChange={handleInputChange}
+                label="On Marketing List"
+                checked={currentUser.onMarketingList}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Button onClick={handleFormSubmit}>Submit</Button>
+            </Form.Group>
+          </Form>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default UserProfileForm;
