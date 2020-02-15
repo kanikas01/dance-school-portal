9509e5c2eda060b7fd7865061b70ffcfea90265e
kanikas01 <larvakurt@yahoo.com>
2020-02-14 17:18:07 -0800

    Change Header to a function component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,15 @@
-import React, { Component,  } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
-class Header extends Component {
-  render () {
-    return (
-      <Jumbotron fluid>
-        <Container className="text-center">
-          <h1>Dance School Portal</h1>
-        </Container>
-      </Jumbotron>
-    );
-  }
+function Header() {
+  return (
+    <Jumbotron fluid>
+      <Container className="text-center">
+        <h1>Dance School Portal</h1>
+      </Container>
+    </Jumbotron>
+  );
 }
 
 export default Header;
