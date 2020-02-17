aa5273f36c9f453e0c1715ee0b559bfc5fcca045
kanikas01 <larvakurt@yahoo.com>
2020-02-17 14:54:25 -0800

    Tweak user grades to hopefully load more gracefully

--- client/src/components/Tabs/UserGrades/UserGrades.js
+++ client/src/components/Tabs/UserGrades/UserGrades.js
@@ -4,53 +4,59 @@ import Table from 'react-bootstrap/Table';
 
 function UserGrades({ userId }) {
   const [grades, setGrades] = React.useState([]);
+  const [fetchComplete, setFetchComplete] = React.useState(false);
 
   React.useEffect(() => {
     gradeAPI
       .getGradesForUser(userId)
-      .then(res => setGrades(res.data))
+      .then(res => {
+        setGrades(res.data);
+        setFetchComplete(true);
+      })
       .catch(err => console.log(err));
   }, [userId]);
 
-  return (
-    <>
-      {grades.length > 0 ? (
-        <>
-          <h3>Grades</h3>
-          <Table bordered hover size="sm">
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Dance</th>
-                <th>Level</th>
-                <th>Score</th>
-                <th>Question Type</th>
-                <th>Detail</th>
-                {/* <th>Comments</th> */}
+  if (grades.length > 0) {
+    return (
+      <>
+        <h3>Grades</h3>
+        <Table bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Dance</th>
+              <th>Level</th>
+              <th>Score</th>
+              <th>Question Type</th>
+              <th>Detail</th>
+              {/* <th>Comments</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {grades.map(grade => (
+              <tr key={grade.id}>
+                <td>{grade.date}</td>
+                <td>{grade.Dance.name}</td>
+                <td>{grade.level}</td>
+                <td>{grade.score}</td>
+                <td>{grade.questionType}</td>
+                <td>{grade.detail}</td>
+                {/* <td>{grade.comment}</td> */}
               </tr>
-            </thead>
-            <tbody>
-              {grades.map(grade => (
-                <tr key={grade.id}>
-                  <td>{grade.date}</td>
-                  <td>{grade.Dance.name}</td>
-                  <td>{grade.level}</td>
-                  <td>{grade.score}</td>
-                  <td>{grade.questionType}</td>
-                  <td>{grade.detail}</td>
-                  {/* <td>{grade.comment}</td> */}
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </>
-      ) : (
-        <>
-          <h3>No Grades Found</h3>
-        </>
-      )}
-    </>
-  );
+            ))}
+          </tbody>
+        </Table>
+      </>
+    );
+  } else if (fetchComplete) {
+    return (
+      <>
+        <h3>No Grades Found</h3>
+      </>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default UserGrades;
