5dc7eeff9fcfe4dd99f9385c7d1ecd38aeba17b1
kanikas01 <larvakurt@yahoo.com>
2020-02-26 18:48:48 -0800

    Update header text and styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,12 +4,20 @@ import Nav from "react-bootstrap/Nav";
 
 function Header() {
   return (
-    <Nav fluid>
+    <Nav>
       <Container className="text-center text-light">
         <h1>Dance School Portal</h1>
-        <h5 id="demo-info" className="text-center">
-          This software is set up in DEMO MODE, which will allow you see the
-          user interface as it would appear to a user of each role.
+        <h5 className="text-left">
+          Dance School Portal is a student/teacher/admin portal for - you
+          guessed it - a dance school. Students can see grades, teachers can
+          grade students, and various other administrative actions are possible
+          depending on the user's role.
+        </h5>
+        <br />
+        <h5 id="demo-info" className="text-left">
+          The app is set up in a Demo mode: you can select any user to view the
+          app as that user. This allows for easy exploration of the user role
+          functionality that controls data acces
         </h5>
       </Container>
     </Nav>
